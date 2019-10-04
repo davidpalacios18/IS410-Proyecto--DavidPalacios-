5ca46a00e9adeed8cc2a7d49778b20de77604168
davidpalacios18 <37627873+davidpalacios18@users.noreply.github.com>
2019-10-04 15:12:18 -0600

    De nuevo

--- Palacios'Store/1-Pagina/js/controlador-empresa.js
+++ Palacios'Store/1-Pagina/js/controlador-empresa.js
@@ -1,17 +1,63 @@
-function initMap(){
-    var ubicacion = new localizacion(()=>{
+$('#agregarProducto').hide();
 
-        var options = {
-            center: {
-                lat: ubicacion.latitude,
-                lng: ubicacion.longitude
-            },
-            zoom: 15
-        }
+function mostrarAgregarProducto(){
+    $('#agregarProducto').show();
+}
 
-        var map = document.getElementById('map');
-        
-        var mapa = new google.maps.Map(map, options);
+function agregarProducto(){
+    let producto = {
+        imagenProducto: $('#imagen').val(),
+        nombreProducto: $('#nombre').val(),
+        descripcionProducto: $('#descripcion').val(),
+        cantidadDisponible: $('#disponible').val(),
+        precioNormal: $('#normal').val(),
+        precioPromocion: $('#promocion').val(),
+        categoriaProducto: $('#categoria').val(),
+    };
+    console.log(producto);
+
+    let formData = new FormData($('#agregarProducto')[0]);
+
+    $.ajax({
+        url: '../ajax/vendedor/'
     });
+    
+    anexarProducto(producto);
+    $('#agregarProducto').hide();
+}
 
+function anexarProducto(producto){
+    $('#contenedorProductos').append(`
+    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 mb-3 caja_margen" >
+        <div class="card text-center ">
+            <div id="imagenProducto" class="card-img-top">
+                <img src="img/productos/" class="img-fluid" alt="...">
+            </div>
+            <div class="card-body tarjeta">
+                <div class="">
+                    <h5 id="tituloProducto" class="card-title">${producto.nombreProducto}</h5>
+                </div>
+                <div id="descripcionProducto" class="">
+                    <small><p id="desProducto" class="card-text truncar-texto">${producto.descripcionProducto}</p></small>
+                    <small><p id="categoria" class="card-text truncar-texto">Categoria: ${producto.categoriaProducto}</p></small>
+                    <div class="card-text row">
+                        <div class="col-6">
+                            <small class="text-muted"><i class="fas fa-tags"></i> <span id="cantidadDisponible">${producto.cantidadDisponible}</span> Disponibles</small>
+                        </div>
+                        <div class="col-6">
+                            <small class="text-muted">Antes<br><span id="precioAntes" style="text-decoration: line-through;"> L ${producto.precioNormal}</span> </small>
+                        </div>
+                    </div><hr>
+                    <div class="">
+                        <p class="card-text"><span id="precioOferta">L ${producto.precioPromocion}</span></p>
+                    </div><hr>
+                </div>
+                <div class="">
+                    <button href="#" class="btn btn-link btn-producto">Editar</button> | <button href="#" class="btn btn-link btn-producto">Eliminar</button>  <!-- btn btn-anadirCarrito -->
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    `);
 }
--- Palacios'Store/1-Pagina/js/localizacion.js
+++ Palacios'Store/1-Pagina/js/localizacion.js
@@ -1,17 +1,37 @@
-class localizacion{
+//let map = L.map('map').setView([-86.5000000, 15.0000000],13);
+/*let latitud = 14.2883000;
+let longitud = -87.0721000;
+
+let map = L.map('map').setView([latitud, longitud],8);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    attribution:'$copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+*/
+window.addEventListener("load", geolocalizar);
+
+function geolocalizar(){
+    navigator.geolocation.getCurrentPosition(encontrado, noEncontrado);
+}
+function encontrado(info){
+    let latitud = info.coords.latitude;
+    let longitud = info.coords.longitude;
+
+    let map = L.map('map').setView([latitud, longitud],11);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+        attribution:'$copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    let marker = L.marker([latitud,longitud]).addTo(map);
     
-    constructor(callback){
-        if(navigator.geolocation){
-            //Obtener Ubicacion
-            navigator.geolocation.getCurrentPosition(
-                (position)=>{
-                    this.latitude = position.coords.latitude;
-                    this.longitude = position.coords.longitude; 
-                    
-                    callback();
-                });
-        }else{
-            alert("Tu navegador no soporta geolocalizacion!")
-        }
-    }
+    console.log("Latitud" + latitud);
+    console.log("Longitud" + longitud);
+    
+    
+}
+
+function noEncontrado(error){
+    console.log('No se encontro la hubicacion');
+    
 }
